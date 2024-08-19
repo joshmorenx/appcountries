65571b9cbd24b0e48cd1777061b3763866f54945
Joshua Moreno <37154328+joshmorenx@users.noreply.github.com>
2024-08-18 21:38:05 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
-  base: '/appcountries/',
-});
-=======
   base:'/appcountries/'
 })
->>>>>>> 2e4638ffdffcd6e0844d1f1612cc7c47754cf246
